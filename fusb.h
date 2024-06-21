d1dee06e800fb85711391d2eb8ed7c3f582fea8b
H3wastooshort <hacker3000@posteo.org>
2024-06-22 01:51:17 +0200

    fix comments

--- fusb.h
+++ fusb.h
@@ -1,8 +1,8 @@
-########################
-#
-# FUSB-specific code
-#
-########################
+////////////////////////////////////////////////
+//
+// FUSB-specific code
+//
+////////////////////////////////////////////////
 
 const uint8_t FUSB302_I2C_SLAVE_ADDR = 0x22;
 const uint8_t TCPC_REG_DEVICE_ID = 0x01;
@@ -39,29 +39,29 @@ FUSB302(I2C_TYPE* new_i2c) {
 }
 
 void reset() {
-	# reset the entire FUSB;
+	// reset the entire FUSB;
 	i2c_dev->writeToRegister(TCPC_REG_RESET, bytes([0b1]));
 }
 
 void reset_pd() {
-	# resets the FUSB PD logic;
+	// resets the FUSB PD logic;
 	i2c_dev->writeToRegister(TCPC_REG_RESET, bytes([0b10]));
 }
 
 void unmask_all() {
-	# unmasks all interrupts;
+	// unmasks all interrupts;
 	i2c_dev->writeToRegister(TCPC_REG_MASK, bytes([0b0]));
 	i2c_dev->writeToRegister(TCPC_REG_MASKA, bytes([0b0]));
 	i2c_dev->writeToRegister(TCPC_REG_MASKB, bytes([0b0]));
 }
 
 uint8_t cc_current() {
-	# show measured CC level interpreted as USB-C current levels;
+	// show measured CC level interpreted as USB-C current levels;
 	return i2c_dev->readFromRegister(TCPC_REG_STATUS0, 1)[0] & 0b11;
 }
 
 void read_cc(cc) {
-	# enable a CC pin for reading;
+	// enable a CC pin for reading;
 	assert(cc in [0, 1, 2]);
 	x = i2c_dev->readFromRegister(TCPC_REG_SWITCHES0, 1)[0];
 	x1 = x;
@@ -69,12 +69,12 @@ void read_cc(cc) {
 	x &= clear_mask;
 	mask = [0b0, 0b100, 0b1000][cc];
 	x |= mask;
-	#//print('TCPC_REG_SWITCHES0: ', bin(x1), bin(x), cc);
+	////print('TCPC_REG_SWITCHES0: ', bin(x1), bin(x), cc);
 	i2c_dev->writeToRegister(TCPC_REG_SWITCHES0, bytes((x,)) );
 }
 
 void enable_pullups() {
-	# enable host pullups on CC pins, disable pulldowns;
+	// enable host pullups on CC pins, disable pulldowns;
 	x = i2c_dev->readFromRegister(0x22, 0x02, 1)[0];
 	x |= 0b11000000;
 	i2c_dev->writeToRegister( 0x02, bytes((x,)) );
@@ -88,7 +88,7 @@ void set_mdac(value) {
 }
 
 void enable_sop() {
-	# enable reception of SOP'/SOP" messages;
+	// enable reception of SOP'/SOP" messages;
 	x = i2c_dev->readFromRegister(TCPC_REG_CONTROL1, 1)[0];
 	mask = 0b1100011;
 	x |= mask;
@@ -109,14 +109,14 @@ void enable_pulldowns() {
 }
 
 uint8_t measure_sink(debug=false) {
-	# read CC pins and see which one senses the pullup;
+	// read CC pins and see which one senses the pullup;
 	read_cc(1);
 	sleep(0.001);
 	cc1_c = cc_current();
 	read_cc(2);
 	sleep(0.001);
 	cc2_c = cc_current();
-	# picking the CC pin depending on which pin can detect a pullup;
+	// picking the CC pin depending on which pin can detect a pullup;
 	cc = [1, 2][cc1_c < cc2_c];
 	//if (debug)
 		//print('m', bin(cc1_c), bin(cc2_c), cc);
@@ -126,7 +126,7 @@ uint8_t measure_sink(debug=false) {
 }
 
 uint8_t measure_source(debug=false) {
-	# read CC pins and see which one senses the correct host current;
+	// read CC pins and see which one senses the correct host current;
 	read_cc(1);
 	sleep(0.001);
 	cc1_c = cc_current();
@@ -145,32 +145,32 @@ uint8_t measure_source(debug=false) {
 }
 
 void set_controls_sink() {
-	# boot: 0b00100100;
-	ctrl0 = 0b00000000 # unmask all interrupts; don't autostart TX.. disable pullup current;
+	// boot: 0b00100100;
+	ctrl0 = 0b00000000 // unmask all interrupts; don't autostart TX.. disable pullup current;
 	i2c_dev->writeToRegister(TCPC_REG_CONTROL0, bytes((ctrl0,)) );
-	# boot: 0b00000110;
-	ctrl3 = 0b00000111 # enable automatic packet retries;
+	// boot: 0b00000110;
+	ctrl3 = 0b00000111 // enable automatic packet retries;
 	i2c_dev->writeToRegister(TCPC_REG_CONTROL3, bytes((ctrl3,)) )
 
 host_current=0b10;
 }
 
 void set_controls_source() {
-	# boot: 0b00100100;
-	ctrl0 = 0b00000000 # unmask all interrupts; don't autostart TX;
-	ctrl0 |= host_current << 2 # set host current advertisement pullups;
+	// boot: 0b00100100;
+	ctrl0 = 0b00000000 // unmask all interrupts; don't autostart TX;
+	ctrl0 |= host_current << 2 // set host current advertisement pullups;
 	i2c_dev->writeToRegister(TCPC_REG_CONTROL0, bytes((ctrl0,)) );
 	i2c_dev->writeToRegister( 0x06, bytes((ctrl0,)) );
-	# boot: 0b00000110;
-	ctrl3 = 0b00000110 # no automatic packet retries;
+	// boot: 0b00000110;
+	ctrl3 = 0b00000110 // no automatic packet retries;
 	i2c_dev->writeToRegister(TCPC_REG_CONTROL3, bytes((ctrl3,)) );
-	# boot: 0b00000010;
-	#ctrl2 = 0b00000000 # disable DRP toggle. setting it to Do Not Use o_o ???;
-	#i2c_dev->writeToRegister(TCPC_REG_CONTROL2, bytes((ctrl2,)) );
+	// boot: 0b00000010;
+	//ctrl2 = 0b00000000 // disable DRP toggle. setting it to Do Not Use o_o ???;
+	//i2c_dev->writeToRegister(TCPC_REG_CONTROL2, bytes((ctrl2,)) );
 }
 
 void set_wake(state) {
-	# boot: 0b00000010;
+	// boot: 0b00000010;
 	ctrl2 = i2c_dev->readFromRegister(0x22, 0x08, 1)[0];
 	clear_mask = ~(1 << 3) & 0xFF;
 	ctrl2 &= clear_mask;
@@ -181,41 +181,41 @@ void set_wake(state) {
 
 void flush_receive() {
 	x = i2c_dev->readFromRegister(TCPC_REG_CONTROL1, 1)[0];
-	mask = 0b100 # flush receive;
+	mask = 0b100 // flush receive;
 	x |= mask;
 	i2c_dev->writeToRegister(TCPC_REG_CONTROL1, bytes((x,)) );
 }
 
 void flush_transmit() {
 	x = i2c_dev->readFromRegister(TCPC_REG_CONTROL0, 1)[0];
-	mask = 0b01000000 # flush transmit;
+	mask = 0b01000000 // flush transmit;
 	x |= mask;
 	i2c_dev->writeToRegister(TCPC_REG_CONTROL0, bytes((x,)) );
 }
 
 void enable_tx(cc) {
-	# enables switch on either CC1 or CC2;
+	// enables switch on either CC1 or CC2;
 	x = i2c_dev->readFromRegister(TCPC_REG_SWITCHES1, 1)[0];
 	x1 = x;
 	mask = cc == 2 ? 0b10 :  0b1;
-	x &= 0b10011100 # clearing both TX bits && revision bits;
+	x &= 0b10011100 // clearing both TX bits && revision bits;
 	x |= mask;
 	x |= 0b100;
-	x |= 0b10 << 5 # revision 3.0;
-	#//print('et', bin(x1), bin(x), cc);
+	x |= 0b10 << 5 // revision 3.0;
+	////print('et', bin(x1), bin(x), cc);
 	i2c_dev->writeToRegister(TCPC_REG_SWITCHES1, bytes((x,)) );
 }
 
 void set_roles(power_role = 0, data_role = 0) {
 	x = i2c_dev->readFromRegister(0x22, 0x03, 1)[0];
-	x &= 0b01101111 # clearing both role bits;
+	x &= 0b01101111 // clearing both role bits;
 	x |= power_role << 7;
 	x |= data_role << 7;
 	i2c_dev->writeToRegister( 0x03, bytes((x,)) );
 }
 
 void power() {
-	# enables all power circuits;
+	// enables all power circuits;
 	x = i2c_dev->readFromRegister(TCPC_REG_POWER, 1)[0];
 	mask = 0b1111;
 	x |= mask;
@@ -223,37 +223,37 @@ void power() {
 }
 
 uint8_t polarity() {
-	# reads polarity and role bits from STATUS1A;
+	// reads polarity and role bits from STATUS1A;
 	return (i2c_dev->readFromRegister(TCPC_REG_STATUS1A, 1)[0] >> 3) & 0b111;
-	#'0b110001';
+	//'0b110001';
 }
 
 uint16_t interrupts() {
-	# return all interrupt registers;
+	// return all interrupt registers;
 	return (i2c_dev->readFromRegister(TCPC_REG_INTERRUPTA, 2) << 8) + i2c_dev->readFromRegister(TCPC_REG_INTERRUPT, 1)
 
-# interrupts are cleared just by reading them, it seems
-#def clear_interrupts() {
-#	# clear interrupt
-#	i2c_dev->writeToRegister(TCPC_REG_INTERRUPTA, bytes([0]))
-#	i2c_dev->writeToRegister(TCPC_REG_INTERRUPT, bytes([0]))
+// interrupts are cleared just by reading them, it seems
+//def clear_interrupts() {
+//	// clear interrupt
+//	i2c_dev->writeToRegister(TCPC_REG_INTERRUPTA, bytes([0]))
+//	i2c_dev->writeToRegister(TCPC_REG_INTERRUPT, bytes([0]))
 
-# this is a way better way to do things than the following function -
-# the read loop should be ported to this function, and the next ome deleted
+// this is a way better way to do things than the following function -
+// the read loop should be ported to this function, and the next ome deleted
 uint8_t rxb_state() {
-	# get read buffer interrupt states - (rx buffer empty, rx buffer full);
+	// get read buffer interrupt states - (rx buffer empty, rx buffer full);
 	st = i2c_dev->readFromRegister(TCPC_REG_STATUS1, 1)[0];
 	return ((st & 0b100000) >> 5, (st & 0b10000) >> 4);
 }
 
-# TODO: yeet
+// TODO: yeet
 uint8_t rxb_state() {
 	st = i2c_dev->readFromRegister(TCPC_REG_STATUS1, 1)[0];
 	return ((st & 0b110000) >> 4, (st & 0b11000000) >> 6);
 }
 
 uint8_t get_rxb(l=80) {
-	# read from FIFO;
+	// read from FIFO;
 	return i2c_dev->readFromRegister(TCPC_REG_FIFOS, l);
 }
 
@@ -269,24 +269,24 @@ uint8_t find_cc(fn=measure_sink, debug=false) {
 	read_cc(cc);
 	flush_transmit();
 	flush_receive();
-	#import gc; gc.collect();
+	//import gc; gc.collect();
 	reset_pd();
 	return cc;
 }
 
-# FUSB toggle logic shorthands
-# currently unused
+// FUSB toggle logic shorthands
+// currently unused
 
 protected:
 const uint8_t polarity_values [][2] = {
-	{0, 0},   # 000: logic still running;
-	{1, 0},   # 001: cc1, src;
-	{2, 0},   # 010: cc2, src;
-	{-1, -1}, # 011: unknown;
-	{-1, -1}, # 100: unknown;
-	{1, 1},   # 101: cc1, snk;
-	{2, 1},   # 110: cc2, snk;
-	{0, 2}    # 111: audio accessory;
+	{0, 0},   // 000: logic still running;
+	{1, 0},   // 001: cc1, src;
+	{2, 0},   // 010: cc2, src;
+	{-1, -1}, // 011: unknown;
+	{-1, -1}, // 100: unknown;
+	{1, 1},   // 101: cc1, snk;
+	{2, 1},   // 110: cc2, snk;
+	{0, 2}    // 111: audio accessory;
 };
 
 const String current_values[] = {
