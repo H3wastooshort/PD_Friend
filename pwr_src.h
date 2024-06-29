a397bcfe1a61176e75db134b5f9738983fcaa3c8
H3wastooshort <hacker3000@posteo.org>
2024-06-29 16:18:20 +0200

    pulled out too much...

--- pwr_src.h
+++ pwr_src.h
@@ -1,1 +1,33 @@
 //stuff only relevant to SRC
+class PDStack_SRC : public PDStack {
+public:
+PDStack_SRC(FUSB302 fusb) : PDStack(fusb) {};
+
+void init_src() {
+  pd.init_universal();
+  fusb->set_controls_source();
+  fusb->set_roles(true, true);
+  fusb->disable_pulldowns();
+}
+
+bool attach_src() {
+  fusb->disable_pulldowns();
+  fusb->set_wake(true);
+  fusb->enable_pullups();
+  fusb->set_mdac(0b111111);
+
+  uint8_t cc = fusb->find_cc_source();
+
+  if (cc == 0) return false;
+  //else
+  fusb->set_cc(cc);
+  return true;
+}
+
+void detach_src() {
+  //fusb->disable_pullups(); //TODO
+}
+
+void set_analog_host_current(uint8_t hc) {
+}
+}
